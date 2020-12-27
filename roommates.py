64f0c5b4417a85130f986f148f15ec74f3274e8b
Tom Wallenstein <wallex@Toms-MBP-6690.fritz.box>
2020-12-27 01:25:43 +0100

    refactoring

--- roommates.py
+++ roommates.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python
-
-
 # Author Tom Wallenstein
 
 import csv
@@ -10,242 +8,201 @@ from PIL import Image, ImageFont, ImageDraw
 from subprocess import Popen
 import os
 import glob
+from pathlib import Path
+import webbrowser
+import pandas
+import math
 
-def getDataFromCSV(file):
-	data = []
-	with open(file, mode='r') as csv_file:
-	    reader = csv.DictReader(csv_file)
-	    for row in reader:
-	    	data.append(row)
-    	
-	return data
+def get_player_info(info_file, pic_folder_path):
+	players = {}
+	with open(info_file, mode='r') as csv_file:
+		reader = csv.DictReader(csv_file)
 
-def printRooms(data):
+		for row in reader:
+			key = f'{row["lastName"]}_{row["firstName"]}'
+			
+			if row['status'] == 'squad':
+				players[key] = row
+				players[key]['picture'] = pic_folder_path + players[key]['picture']
+
+	return players
+
+
+def print_rooms(assignments):
 	print('---- ROOM ASSIGNMENTS ----')
-	for element in data:
-		print(element)
+	for room in assignments:
+		names = [f"{p['firstName']} {p['lastName']}" for p in room]
+		print(', '.join(names))
+		print('-----')
 	return
 
 
-def getImageRoom2(path1, path2, name1, name2):	
-	image_width = 1100
-	image_height = 1100 
+def get_random_roommates(players, room_cap):
+	room_mates = []
+	for i in range(room_cap):
+		random_player = random.choice(players)
+		room_mates.append(random_player)
+		players.remove(random_player)
 
-	# print('Path1 :' + path1)
-	# print('Path2 :' + path2)
-	try:
+		if len(players) == 0:
+			break
+	return players, room_mates
 
-   		img1  = Image.open(path1) 
-   		width, height = img1.size 
-		# print("image1 opening successful")
-   		if (width != image_width and height != image_height):
-   			img1 = img1.resize((image_width, image_height)) 
 
-	except IOError: 
-		pass
+def get_room_assignments(rooms, player_dict):
+	room_assignments = []
 
-	try:  
-   		img2  = Image.open(path2) 
-   		width, height = img2.size 
-   		# print("image2 opening successful")
-   		if (width != image_width and height != image_height):
-   			img2 = img2.resize((image_width, image_height)) 
+	r_caps = sorted(rooms.keys(), reverse=True)
+	players = list(player_dict.values())
+
+	for cap in r_caps:
+		while(rooms[cap] != 0):
+			players, room = get_random_roommates(players, int(cap))
+			rooms[cap] -= 1
+			room_assignments.append(room)
+
+			if len(players) == 0:
+				break
+
+	return room_assignments
+
+def create_image(img_paths, names):
+	canvas_width, canvas_height = 2560, 1600
+	new_im = Image.new('RGB', (canvas_width, canvas_height), 'black')
 	
-	except IOError: 
-		pass
+	max_imgs_in_row = 4
+	n_imgs = len(img_paths)
 
-	w = 2560
-	h = 1600
-	padding_side = (w/2 - image_width) / 2
-	padding_top = (h - image_height) / 2
-
-	endImg = Image.new('RGBA', (2560, 1600), 'black')
-	endImg.paste(img1, (padding_side, padding_top))
-	endImg.paste(img2, (w/2+padding_side, padding_top))
-
-	draw = ImageDraw.Draw(endImg)
-	font = ImageFont.truetype("~/Library/Fonts/Arial.ttf", 160)
-
-	if (name1 == 'Hot Aldo'):
-		draw.text((350, 110) , name1,(255,0,0), font=font)
-
-	# draw = ImageDraw.Draw(endImg)
-	# font = ImageFont.truetype("~/Library/Fonts/Arial.ttf", 36)
-	if (name2 == 'Hot Aldo'):
-		draw.text((1600, 1150),name2,(255,0,0), font=font)
-
-	return endImg
-
-def getImageRoom3(path1, path2, path3, name1, name2, name3):	
-	image_width = 800
-	image_height = 800 
-
-	try:  
-   		img1  = Image.open(path1) 
-   		width, height = img1.size 
-
-   		if (width != image_width and height != image_height):
-   			img1 = img1.resize((image_width, image_height)) 
-
-	except IOError: 
-		pass
-
-	try:  
-   		img2  = Image.open(path2) 
-   		width, height = img2.size 
-
-   		if (width != image_width and height != image_height):
-   			img2 = img2.resize((image_width, image_height)) 
-	
-	except IOError: 
-		pass
-
-	try:  
-   		img3  = Image.open(path3) 
-   		width, height = img3.size 
-
-   		if (width != image_width and height != image_height):
-   			img3 = img3.resize((image_width, image_height)) 
-	
-	except IOError: 
-		pass
-
-	w = 2560
-	h = 1600
-	padding_side = (w - 3 * image_width) / 6
-	padding_top = (h - image_height) / 2
-
-	endImg = Image.new('RGBA', (2560, 1600), 'black')
-	endImg.paste(img1, (padding_side, padding_top))
-	endImg.paste(img2, (image_width + 3 * padding_side, padding_top))
-	endImg.paste(img3, (2 * image_width + 5 * padding_side, padding_top))
-
-	draw = ImageDraw.Draw(endImg)
-	font = ImageFont.truetype("~/Library/Fonts/Arial.ttf", 36)
-
-	if (name1 == 'Hot Aldo'):
-		draw.text((133, 480) , name1,(255,255,255), font=font)
-
-	# draw = ImageDraw.Draw(endImg)
-	# font = ImageFont.truetype("~/Library/Fonts/Arial.ttf", 36)
-	if (name2 == 'Hot Aldo'):
-		draw.text((420, 480),name2,(255,255,255), font=font)
-
-	# draw = ImageDraw.Draw(endImg)
-	# font = ImageFont.truetype("~/Library/Fonts/Arial.ttf", 36)
-	
-	if (name3 == 'Hot Aldo'):
-		draw.text((760, 480),name3,(255,255,255), font=font)
-
-	return endImg
-
-
-squad = getDataFromCSV('squad.txt')
-num_Players = len(squad)
-
-nicknames = getDataFromCSV('nicknames.txt')
-
-# print "Squad"
-# for row in squad:
-# 	   	print(row)
-
-# print "Nicknames"
-# for row in nicknames:
-# 	   	print(row)
-
-room_assignments = []
-
-while (squad != []):
-	if (len(squad) != 3):
-		first = random.choice(squad)
-		second = random.choice(squad)
-
-		while (first == second):
-			second = random.choice(squad)
-
-		room_assignments.append([first["lastName"], second["lastName"]])
-		squad.remove(first)
-		squad.remove(second)
-	else:
-		first = random.choice(squad)
-		second = random.choice(squad)
-		third = random.choice(squad)
-
-		while (first == second):
-			second = random.choice(squad)
-		while (first == third or second == third):
-			third = random.choice(squad)
-
-		room_assignments.append([first["lastName"], second["lastName"], third["lastName"]])
-		squad.remove(first)
-		squad.remove(second)
-		squad.remove(third)
-
-# print(room_assignments)
-
-path_application = '/Applications/Preview.app'
-
-
-wait = Image.new('RGBA', (2560, 1600), 'black')
-wait.save("./temp/wait.png")
-p = Popen(['open', path_application, "./temp/wait.png"])
-time.sleep(6)
-	
-p.terminate()
-p.kill()
-# raw_input("Press Enter to continue...")
-
-for room in room_assignments:
-	if len(room) == 2:
-		img1_path = "./pics/" + room[0] + ".jpg"
-		img2_path = "./pics/" + room[1] + ".jpg"
+	if n_imgs <= max_imgs_in_row:
+		box_width = canvas_width // n_imgs
+		box_height = canvas_height
+	else: 
+		box_width = canvas_width // max_imgs_in_row
 		
-		nickname1 = filter(lambda x: x["name"] == room[0], nicknames)[0]["nickname"]
-		nickname2 = filter(lambda x: x["name"] == room[1], nicknames)[0]["nickname"]
+		n_rows = math.ceil(n_imgs // max_imgs_in_row)
+		box_height = canvas_height // n_rows
 
-		
-		# print('path1 ' + img1_path)
-		# print('path2 ' + img2_path)
-		img = getImageRoom2(img1_path, img2_path, nickname1, nickname2)
+	padding_sides = 10
+	padding_top = 30
+	dist_text = 30
 
-		saveName = "./temp/"+room[0]+room[1]+".png"
-		img.save(saveName)
-	else:
-		img1_path = "./pics/" + room[0] + ".jpg"
-		img2_path = "./pics/" + room[1] + ".jpg"
-		img3_path = "./pics/" + room[2] + ".jpg"
+	img_size = min([box_width-(padding_sides*2), box_height])
 
-		# print('path1 ' + img1_path)
-		# print('path2 ' + img2_path)
-		# print('path3 ' + img3_path)
+	draw = ImageDraw.Draw(new_im)
+	font = ImageFont.truetype("~/Library/Fonts/Arial.ttf", 72)
+	text = "Test"
+	text_width, text_height = draw.textsize(text, font)
 
-		nickname1 = filter(lambda x: x["name"] == room[0], nicknames)[0]["nickname"]
-		nickname2 = filter(lambda x: x["name"] == room[1], nicknames)[0]["nickname"]
-		nickname3 = filter(lambda x: x["name"] == room[2], nicknames)[0]["nickname"]
-
-		img = getImageRoom3(img1_path, img2_path, img3_path, nickname1, nickname2, nickname3)
-
-		saveName = "./temp/"+room[0]+room[1]+room[2]+".png"
-		img.save(saveName)
-
-	p = Popen(['open', path_application, saveName])
-	time.sleep(12)
-	# raw_input("Press Enter to continue...")
+	h_img_txt = img_size + dist_text + text_height
+	padding_top = (box_height - h_img_txt) // 2
 	
-	# p.terminate()
-	# p.kill()
-	# time.sleep(1)
+	images = [Image.open(path) for path in img_paths]
 
-printRooms(room_assignments)
+	for i, im in enumerate(images):
+		im = im.resize((img_size, img_size))
 
-# Get a list of all the file paths that ends with .txt from in specified directory
-fileList = glob.glob('./temp/*.png')
-# Iterate over the list of filepaths & remove each file.
-for filePath in fileList:
-    try:
-        os.remove(filePath)
-    except:
-        print("Error while deleting file : ", filePath)
+		row = i // max_imgs_in_row
+		col = i % max_imgs_in_row
+
+		x = col * box_width + padding_sides
+		y = row * box_height + padding_top
+
+		new_im.paste(im, (x, y))
+
+		draw = ImageDraw.Draw(new_im)
+		text = names[i]
+		text_width, text_height = draw.textsize(text, font)
+		color = (255, 255, 255)
+		x_pos = col * box_width + ((box_width - text_width) // 2)
+		y_pos = row * box_height + padding_top + img_size + dist_text
+		draw.text((x_pos, y_pos), text, color, font=font)
+
+	return new_im
+
+
+def show_rooms(room_assignments, main_folder, start_wait, transition_time):
+	home = Path('.')
+	wait = Image.new('RGBA', (2560, 1600), 'black')
+	wait.save("./temp/wait.png")
+
+	webbrowser.open("file://" + str(home.resolve()) + "/temp/wait.png")
+	time.sleep(start_wait)
+
+	for room in room_assignments:
+		imgs = [player['picture'] for player in room]
+		names = [player['nickname'] for player in room]
+		outfile = '_'.join([player['lastName'] for player in room])
+		
+		img = create_image(imgs, names)
+
+		saveName = "./temp/"+outfile+".png"
+		img.save(saveName)
+		
+		webbrowser.open_new_tab("file://" + str(home.resolve()) + saveName[1:])
+		time.sleep(transition_time)
+
+
+def clean_up():
+	# Get a list of all the file paths that ends with .txt from in specified directory
+	fileList = glob.glob('./temp/*.png')
+	# Iterate over the list of filepaths & remove each file.
+	for filePath in fileList:
+	    try:
+	        os.remove(filePath)
+	    except:
+	        print("Error while deleting file : ", filePath)
+
+
+def get_room_configurations():
+	quit_signal = False
+	rooms = {}
+
+	while(not quit_signal):
+		print('-- NEW ROOM CONFIGURATION --')
+		print('Room Size (# of people): ')
+		cap = input()
+		print('Number of Rooms: ')
+		num = input()
+
+		rooms[cap] = int(num)
+
+		while(True):
+			print('*******')
+			print('Press [n] to enter another room configuration')
+			print('Press [d] to continue with room assignments')
+			print('Press [q] to quit programm')
+		
+			key = input()
+			if key == 'd':
+				quit_signal = True
+				break
+			elif key == 'n':
+				break
+			elif key == 'q':
+				exit()
+			else:
+				print('Invalid Input! Try again!')
+
+	return rooms
+
+# main function
+def main():
+	main_folder = './'
+	# get number and sizes of rooms
+	rooms = get_room_configurations()
+	# get roster of players
+	squad = get_player_info(info_file='squad.csv', pic_folder_path=main_folder + 'pics/')
+
+	room_assignments = get_room_assignments(rooms, squad)
+
+	show_rooms(room_assignments, main_folder=main_folder, start_wait=3, transition_time=5)
+
+	print_rooms(room_assignments)
+	clean_up()
+		
+
+if __name__ == '__main__':
+	main()
 
 
 
